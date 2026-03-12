1a18859fa1c7412b6d122bb77786bef3361c2d9c
bvmks <lian.ttr@gmail.com>
2026-03-12 13:48:47 +0300

    first commit

---
+++ liblyc.c
@@ -0,0 +1,35 @@
+#include "liblyc.h"
+#include <endian.h>
+#include <sys/time.h>
+
+int  ms_pack_header(struct ms_msg_header* src, void* dst)
+{
+    char* ptr = dst;
+    *((_LIBLYC_MESG_T*)ptr) = htobe16(src->type);
+    ptr += sizeof(_LIBLYC_MESG_T);
+    *((_LIBLYC_HASH_T*)ptr) = htobe32(src->hash);
+    ptr += sizeof(_LIBLYC_HASH_T);
+    *((_LIBLYC_TMST_T*)ptr) = htobe64(src->timestamp);
+    ptr += sizeof(_LIBLYC_TMST_T);
+    return (ptr - (char*)dst);
+}
+
+int  ms_unpack_header(void* src, struct ms_msg_header* dst)
+{
+    char* ptr = src;
+    dst->type = be16toh(*((_LIBLYC_MESG_T*)ptr));
+    ptr += sizeof(_LIBLYC_MESG_T);
+    dst->hash = be32toh(*((_LIBLYC_HASH_T*)ptr));
+    ptr += sizeof(_LIBLYC_HASH_T);
+    dst->timestamp = be64toh(*((_LIBLYC_TMST_T*)ptr));
+    ptr += sizeof(_LIBLYC_TMST_T);
+    return (ptr - (char*)src);
+}
+
+_LIBLYC_TMST_T get_timestamp()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (unsigned long)tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
---
+++ liblyc.h
@@ -0,0 +1,63 @@
+/*
+ * 
+ *
+ *
+ * 
+ */
+
+
+
+#ifndef _LIBLYC_H
+#define _LIBLYC_H
+
+#include <time.h>
+
+#define _LIBLYC_TMST_T unsigned long
+#define _LIBLYC_MESG_T unsigned short
+#define _LIBLYC_HASH_T int
+
+
+#define MS_MSG 0
+
+
+#define _LYC_HEADER_IN(type, name) type name;
+
+#define _HEADER_LIST \
+        _LYC_HEADER_IN(_LIBLYC_MESG_T, type)\
+        _LYC_HEADER_IN(_LIBLYC_HASH_T, hash)\
+        _LYC_HEADER_IN(_LIBLYC_TMST_T, timestamp)
+    
+
+/*
+ *  basic MS protocol header
+ */
+struct ms_msg_header {
+    _HEADER_LIST
+};
+
+#define _LYC_HEADER_IN(type, ...) sizeof(type) +
+
+enum {
+    ms_header_len = (_HEADER_LIST 0)
+};
+
+/*
+ *  serializes MS header into memory region
+ *  at least expected sizeof(ms_msg_header) bytes of memory
+ *  total amount of written bytes may be less
+*/
+int ms_pack_header(struct ms_msg_header* src, void* dst);
+
+/*
+ *  deserializes MS header from memory region into structure
+*/
+int ms_unpack_header(void* src, struct ms_msg_header* dst);
+
+
+/*
+ *  returns time in milliseconds since Epoch
+*/
+_LIBLYC_TMST_T get_timestamp();
+
+
+#endif // !_LIBLYC_H
