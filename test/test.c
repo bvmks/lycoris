eec3d1625ffabf438971b157336784046f5d41cc
JVSTFXRSH <samsungj510030178@gmail.com>
2026-03-12 15:28:19 +0300

    tests

---
+++ test/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "../liblyc.h"
+#include <endian.h>
+
+
+int main(int argc, char** argv)
+{
+    
+
+}
+
+
