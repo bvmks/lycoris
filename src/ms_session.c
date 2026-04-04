4e60b1076d202a99045eb77f6639073db6af24f0
sh13a <lian.ttr@gmail.com>
2026-04-04 16:33:44 +0300

    added list to sessions

--- src/ms_session.c
+++ src/ms_session.c
@@ -11,56 +11,6 @@ unsigned short generate_id(unsigned int key)
 void mss_init_session(struct ms_session* session, unsigned int skey)
 {
     session->id = generate_id(skey);
-    session->mask_conf.mask = 0;
-    session->mask_conf.last_seq = 0;
-    session->mask_recvd.mask = 0;
-    session->mask_recvd.last_seq = 0;
-}
-
-int ms_mask_check(unsigned short seq, struct ms_mask* mask)
-{
-    short diff = (short)(seq - mask->last_seq);
-    if (diff > 0)
-        return 0;
-    else if (-diff >= (short)(sizeof(mask->mask) * 8))
-        return -1;
-    if (mask->mask & (1ULL << -diff))
-        return 1;
-    else
-        return 0;
-}
-
-int ms_mask_add(unsigned short seq, struct ms_mask* mask) 
-{
-    short diff = (short)(seq - mask->last_seq);
-    unsigned long long m;
-    if (diff > 0) {
-        if (diff >= (short)(sizeof(mask->mask) * 8)) {
-            mask->mask = 1ULL;
-        }
-        else {
-            mask->mask <<= diff;
-            mask->mask |= 1ULL;
-        }
-        mask->last_seq = seq;
-        return 0;
-    }
-
-    if (-diff >= (short)(sizeof(mask->mask) * 8))
-        return -1;
-
-    m = (1ULL << -diff);
-    if (mask->mask & m)
-        return 1;
-    else {
-        mask->mask |= m;
-        return 0;
-    }
-}
-
-int ms_mask_inc(struct ms_mask* mask)
-{
-    mask->mask <<= 1;
-    mask->last_seq++;
-    return 0;
+    mss_init_list(&session->recvd);
+    mss_init_list(&session->sent);
 }
--- src/ms_session.h
+++ src/ms_session.h
@@ -1,37 +1,20 @@
 #ifndef _MS_SESSION_H
 #define _MS_SESSION_H
 
+#include "ms_list.h"
+
 enum {
     ms_session_timeout = 3,
     ms_start_seq = 0
 };
 
 struct ms_session {
-    struct ms_mask {
-        unsigned long long mask; /* mask for last 64 packages*/
-        unsigned short last_seq; /* sequence number of last package */
-    } mask_conf, mask_recvd;
+    struct _ms_list recvd, sent;
     unsigned short id; /* session identifier */
 };
 
 unsigned short generate_id(unsigned int key);
 
 void mss_init_session(struct ms_session* session, unsigned int skey);
-
-/* 
- * checks mask if seq is duplicate 
- * if so returns 1, otherwise 0, on error returns -1
- * error means message have sequence num older than 64, and cannot be checked
- */
-int ms_mask_check(unsigned short seq, struct ms_mask* mask);
-
-/*
- * marks sequence num in mask
- * if it`s already marked returns 1, otherwise 0, on error returns -1
- * error means message have sequence num older than 64, and cannot be marked
- */
-int ms_mask_add(unsigned short seq, struct ms_mask* mask);
-
-int ms_mask_inc(struct ms_mask* mask);
 
 #endif
