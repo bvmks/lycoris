35ab1d26b41b26565010e3790e173144abf68da6
sh13a <lian.ttr@gmail.com>
2026-04-08 00:32:40 +0300

    fixed ms_recv_packet

--- src/ms_proto.c
+++ src/ms_proto.c
@@ -46,6 +46,7 @@ int ms_recv_packet(int sockfd, struct ms_received_packet* p)
     socklen_t addr_len = sizeof(addr);
     int recvd;
     recvd = recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr*)&addr, &addr_len);
+    sockaddr_in2addrport(&p->src_addr, &addr );
     if(recvd == -1)
         return recvd;
     ms_parse(buf, sizeof(buf), &p->packet.header, &p->packet.data, &p->packet.data_size);
