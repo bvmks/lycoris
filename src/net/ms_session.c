d24a3f74f047126285948f8911f685fea9e64eb4
sh13a <lian.ttr@gmail.com>
2026-04-15 21:02:43 +0300

    also 'dovel do uma', but still some work needed

--- src/net/ms_session.c
+++ src/net/ms_session.c
@@ -1,6 +1,32 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "ms_session.h"
+#include "utils.h"
+
+
+struct ms_udp_session* make_udp_session()
+{
+    struct ms_udp_session* s;
+    s = malloc(sizeof(*s));
+    s->created_at = 0;
+    s->last_activity = 0;
+    s->hctx = NULL;
+    ms_nonce_init_rand(&s->nonce);
+    s->remote_addr.addr = 0;
+    s->remote_addr.port = 0;
+    memset(s->rx_key, 0 , rx_key_size);
+    memset(s->tx_key, 0 , tx_key_size);
+    return s;
+}
+
+void dispose_udp_session(struct ms_udp_session* s);
+
+
+void ms_udp_session_init(struct ms_udp_session *s)
+{
+
+}
 
 unsigned short generate_id(unsigned int key)
 {
--- src/net/ms_session.h
+++ src/net/ms_session.h
@@ -20,6 +20,7 @@ enum {
 };
 
 enum ms_us_state {
+    ms_us_stub,
     ms_us_init,
     ms_us_handshake,
     ms_us_active,
@@ -27,15 +28,17 @@ enum ms_us_state {
     ms_us_terminated,
 };
 
-#define MSS_COMMON(name) char name;
-
 struct ms_udp_session {
     int sockfd;
     struct addrport remote_addr;
     enum ms_us_state state;
+
     int side; /* a.k.a we are server or client */
     struct ms_handshake_ctx* hctx;
-    struct ms_crypto_comm_ctx ctx;
+
+    unsigned char rx_key[rx_key_size];
+    unsigned char tx_key[tx_key_size];
+    struct ms_nonce nonce;
 
     unsigned long created_at;
     unsigned long last_activity;
@@ -49,17 +52,11 @@ struct ms_session {
 };
 
 
-/*
- * used to generate random number 
- * based on key specified
-*/
-unsigned short generate_id(unsigned int key);
+struct ms_udp_session* make_udp_session();
+void dispose_udp_session(struct ms_udp_session* s);
 
-/*
-*  used to generate session
-*/
+void ms_udp_session_init(struct ms_udp_session* s);
 
-void mss_udp_session_init(struct ms_udp_session* s, unsigned short id);
 
 void session_init(struct ms_session* session, unsigned short id);
 
