bf790e060321c1ecd9019ff60f1e35e236535b99
sh13a <lian.ttr@gmail.com>
2026-04-29 07:41:03 +0300

    made normal main loop scheme +-

--- test/cryp_server_test.c
+++ test/cryp_server_test.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
@@ -5,26 +6,72 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <time.h>
+#include <sys/select.h>
 
 #include "../src/net/ms_proto.h"
 
 #include "../src/fileutil.h"
 #include "../src/message.h"
 #include "../src/hexdata.h"
+#include "../src/events.h"
+#include "../src/net/ms_rx.h"
 
 enum {
-    def_port = 24880
+    def_port = 24880,
+    select_timeout_usec = 1000000,
 };
 
+struct signal_target {
+    struct signal_handler siginth;
+};
+
+static void calculate_timeout(struct ms_node* n, struct timespec* tm) 
+{
+    /* 
+     * for now it's stupid const value
+     * must be calculated depending on peers timeouts
+     * will make later (at least believe so)
+     */
+    tm->tv_sec = 0;
+    tm->tv_nsec = select_timeout_usec;
+}
+
+static void prepare_sig_handlers(struct sig_handler_item** list) {
+    struct signal_handler h;
+    register_signal(list,&h);
+}
 
 static int main_loop(struct ms_node* node)
 {
-    int loop, result;
-    loop = 0;
-    result = 0;
-    
+    int res = 0, result = 0, needbreak = 0;
+    struct sig_handler_item* sig_handlers = NULL;
+    prepare_sig_handlers(&sig_handlers);
+
     fprintf(stderr, "[DEBUG] Enterring main loop\n");
-    while(loop) {
+    while(!needbreak) {
+        fd_set rd_fds;
+        sigset_t ps_mask;
+        struct timespec rd_timeout;
+
+        FD_ZERO(&rd_fds);
+        FD_SET(node->fd, &rd_fds);
+
+        enter_signal_section(sig_handlers, &ps_mask);
+        handle_signal_events(sig_handlers);
+
+        calculate_timeout(node, &rd_timeout);
+        res = pselect(node->fd + 1, &rd_fds, NULL, NULL, &rd_timeout, &ps_mask);
+        if(res == -1 && errno != EINTR)
+            return -1;
+
+        if(res == -1 && errno == EINTR)
+            handle_signal_events(sig_handlers);
+        leave_signal_section(&ps_mask);
+
+        if(res > 0) {
+            do_recv(node);
+        }
+
     }
     fprintf(stderr, "[DEBUG] Quitting main loop\n");
     return result;
@@ -51,6 +98,7 @@ static int ensure_wdir()
 
 int main(int argc, char** argv)
 {
+    struct event_selector selector;
     struct ms_node* node;
     struct ms_node_cfg* node_cfg;
     
@@ -71,6 +119,9 @@ int main(int argc, char** argv)
     }
 
     message(mlv_normal, "Started node: %s\n", hexdata2a(node->id->node_id, node_id_size));
+
+    sel_init(&selector);
+
     start_node(node);
     
     return main_loop(node);
