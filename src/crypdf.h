3dbf83cf0d993249685ef146b7c162f911f53fed
sh13a <lian.ttr@gmail.com>
2026-05-04 23:57:48 +0300

    some new defines

--- src/crypdf.h
+++ src/crypdf.h
@@ -12,11 +12,18 @@ enum {
     kex_secret_size = 32,
     kex_public_size = 32,
     cipher_key_size = 32,
+
+    sign_size,
+
+    nonce_total = 24,
+    nonce_used = 8,
+    nonce_offset = nonce_total - nonce_used,
     
     node_secret_size = 32,
 
 
     node_id_size = 10,
+    node_name_max_size = 30,
 
 };
 
