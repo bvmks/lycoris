fb76f8e86fe7c3450a1d537c8887a4c005454ddf
sh13a <lian.ttr@gmail.com>
2026-05-04 23:57:13 +0300

    added some needed stuff

--- src/ms_txq.h
+++ src/ms_txq.h
@@ -19,19 +19,26 @@ struct ms_transmit_queue {
     struct ms_transmit_item *first, *last;
 };
 
-struct ms_transmit_queue* make_transmit_queue(struct sue_event_selector* s);
+struct ms_transmit_queue* 
+make_transmit_queue(struct sue_event_selector* s);
 
 void ms_txq_enqueue(struct ms_transmit_item* item);
 
-int txq_want_write(const struct ms_transmit_queue *txq);
+int txq_want_write(const struct ms_transmit_queue* txq);
 
-void ms_txq_enqueue(struct ms_transmit_item* item);
+struct ms_transmit_item* 
+get_item_to_transmit(struct ms_transmit_queue *txq);
 
-struct ms_transmit_item *make_txitem_4peer(struct ms_transmit_queue *txq,
-                                    int len, int offset, struct ms_peer *peer);
+void ms_txq_item_sent(struct ms_transmit_item* item);
 
- 
-struct ms_transmit_item *make_txitem_4ip(struct ms_transmit_queue* txq,
-                         int len, int offset, unsigned int ip, unsigned short port);
+struct ms_transmit_item* 
+make_txitem_4peer(struct ms_transmit_queue* txq,
+                  int len, int offset, 
+                  struct ms_peer* peer);
+
+struct ms_transmit_item* 
+make_txitem_4ip(struct ms_transmit_queue* txq,
+                int len, int offset,
+                unsigned int ip, unsigned short port);
 
 #endif
